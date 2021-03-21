e3b0b8367daaa0f1db055c13f94fb015f4ff28e0
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2021-03-21 12:53:34 -0300

    feat: Reorganizando repositorio

--- 02-test-enext-backend/src/main/java/enext/backend/test/tasks/Task1.java
+++
@@ -1,8 +0,0 @@
-package enext.backend.test.tasks;
-
-public class Task1 {
-    public static void executar() {
-        for (int i = 0, len = LogParser.getGames().size(); i < len; i++)
-            System.out.println(LogParser.getGames().get(i).gameReport(i+1));
-    }
-}
