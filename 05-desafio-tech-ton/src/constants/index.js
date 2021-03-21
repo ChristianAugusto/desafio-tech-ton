6063229cb2baafad2f9b4085dd72521c2a7618a8
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2021-03-21 01:18:51 -0300

    feat: Finalizando primeira versão rotas gets

--- 05-desafio-tech-ton/src/constants/index.js
+++ 05-desafio-tech-ton/src/constants/index.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.port || 3000,
-    ENVIRONMENT: process.env.environment || 'dev'
+    ENVIRONMENT: process.env.environment || 'dev',
+    DATE_FORMAT_MYSQL: 'YYYY-MM-DD',
+    SERVER_TIMEZONE: 'America/Sao_Paulo',
+    EMPLOYEES_LIMIT: 50
 };
--- 05-desafio-tech-ton/src/routes/employees/index.js
+++ 05-desafio-tech-ton/src/routes/employees/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const moment = require('moment-timezone');
 const mysql = require('../../repositories/mysql');
 const numberInStr = require('../../utils/number-in-str');
+const deleteArrayPosition = require('../../utils/delete-array-position');
 const loggerHandler = require('../../utils/logger');
+const { EMPLOYEES_LIMIT } = require('../../constants');
 const logger = loggerHandler.create();
 
 
@@ -17,15 +20,42 @@ function SELECT_EMPLOYEES_QUERY_BUILDER(fields='*', conditions='', limit='') {
 
 router.get('/', async function(req, res) {
     try {
-        res.status(200).json({
-            data: null,
+        let fields = req.query.fields ? 
+            req.query.fields.split(',').map((field) => `\`${field}\``).join(',')
+            :
+            '*';
+
+        let startIndex = 0;
+        if (numberInStr(req.query.startIndex)) {
+            startIndex = Number(req.query.startIndex);
+        }
+
+        const query = SELECT_EMPLOYEES_QUERY_BUILDER(fields, '', `LIMIT ${startIndex},${EMPLOYEES_LIMIT}`);
+
+        const queryResult = await mysql(query);
+
+
+        if (fields === '*' || fields.indexOf('birthDate') != -1) {
+            for (let i = 0; i < queryResult.length; i++) {
+                queryResult[i].birthDate = new Date(queryResult[i].birthDate).toISOString().split('T')[0];
+            }
+        }
+
+
+        return res.status(200).json({
+            data: queryResult,
             message: 'Success'
         }).end();
     }
     catch (error) {
-        res.status(500).json({
+        logger.info(`Error in GET employees, ${JSON.stringify({
+            query: req.query,
+            body: req.body,
+            error: error.message || error.data || error.error || error
+        })}`);
+        return res.status(500).json({
             data: error.message || error.data || error.error || error,
-            message: 'Error on process checkout notification'
+            message: 'Error in GET employees'
         }).end();
     }
 });
@@ -35,42 +65,60 @@ router.get('/:id', async function(req, res) {
         const id = req.params.id;
 
         if (!numberInStr(id)) {
-            res.status(400).json({
+            return res.status(400).json({
                 data: null,
                 message: 'Invalid id type, check if it is number'
             }).end();
         }
 
 
-        const fields = req.query.fields ? 
+        let fields = req.query.fields ? 
             req.query.fields.split(',').map((field) => `\`${field}\``).join(',')
             :
             '*';
 
         const query = SELECT_EMPLOYEES_QUERY_BUILDER(fields, `WHERE \`id\` = ${id}`);
 
-        const result = await mysql(query);
+        const queryResult = await mysql(query);
+
+        if (queryResult.length === 0) {
+            return res.status(200).json({
+                data: null,
+                message: 'There is not employee for given id'
+            }).end();
+        }
+
+        const employee = queryResult[0];
 
 
-        res.status(200).json({
-            data: result[0],
+        if (employee.birthDate) {
+            /*
+                Formating date mysql to YYYYY-MM-DD
+            */
+            employee.birthDate = new Date(employee.birthDate).toISOString().split('T')[0];
+        }
+
+
+        return res.status(200).json({
+            data: employee,
             message: 'Success'
         }).end();
     }
     catch (error) {
         logger.info(`Error in GET employees by id, ${JSON.stringify({
             query: req.query,
-            body: req.body
+            body: req.body,
+            error: error.message || error.data || error.error || error
         })}`);
-        res.status(500).json({
+        return res.status(500).json({
             data: error.message || error.data || error.error || error,
-            message: 'Error on process checkout notification'
+            message: 'Error in GET employees by id'
         }).end();
     }
 });
 
 router.post('/', function(req, res) {
-    res.status(200).json({
+    return res.status(200).json({
         foo: 'foo'
     }).end();
 });
