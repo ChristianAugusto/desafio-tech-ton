5e8bf9c1865116d9989e618b2d8c2736dbdbe360
ChristianAugusto <51584076+ChristianAugusto@users.noreply.github.com>
2021-03-23 11:40:25 -0300

    feat: Atualizando meu IP público

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         MYSQL_DATABASE: 'desafio-tech-ton'
     },
     heroku: {
-        MYSQL_HOST: '179.145.58.147',
+        MYSQL_HOST: '191.13.160.144',
         MYSQL_PORT: '3306',
         MYSQL_USER: 'root',
         MYSQL_PASSWORD: 'tFdDwvN4gmtoiFnNFb6I72UXlpuydD0JZF9m3cL8dD9LmJHt66',
